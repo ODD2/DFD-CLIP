bc0e5aeaa0086a775d89a91f1e5b90c07fe872c0
OD <vhanxtreme@gmail.com>
2023-04-11 16:39:24 +0800

    1. additional scripts for inferences 2. modified the rPPG dataset for multi compression source videos. 3. bug fix

--- src/callbacks/tracking.py
+++ src/callbacks/tracking.py
@@ -27,7 +27,7 @@ def add_main_metric(agent):
 @torch.no_grad()
 def cache_best_model(agent):
     target_metrics = [value for name,value in agent.compute_metrics.items() if  agent.main_metric in name]
-    main_metric =  sum(target_metrics) / max(len(target_metrics) ,0)
+    main_metric =  sum(target_metrics) / max(len(target_metrics) ,1)
     current_best = getattr(agent, 'best_main_metric', main_metric)
 
     if getattr(builtins, agent.compare_fn)(main_metric, current_best) == main_metric:
--- src/datasets.py
+++ src/datasets.py
@@ -18,184 +18,12 @@ import xml.etree.ElementTree as ET
 from glob import glob
 from scipy.signal import resample
 from pyedflib import highlevel as reader
-
 import logging
 
-
-# class FFPP(Dataset):
-#     @staticmethod
-#     def get_default_config():
-#         C = CN()
-#         C.name = 'train'
-#         C.root_dir = './datasets/ffpp/'
-#         C.types = ['REAL', 'DF']
-#         C.compressions = ['raw']
-#         C.detection_level = 'video'
-#         C.dataset = "FFPP"
-#         return C
-
-#     def __init__(self, config,num_frames,clip_duration, transform, accelerator, split='train'):
-#         self.TYPE_DIRS = {
-#             'REAL': 'real/',
-#             # 'DFD' : 'data/original_sequences/actors/',
-#             'DF'  : 'DF/',
-#             'FS'  : 'FS/',
-#             'F2F' : 'F2F/',
-#             'NT'  : 'NT/',
-#             # 'FSH' : 'data/manipulated_sequences/FaceShifter/',
-#             # 'DFD-FAKE' : 'data/manipulated_sequences/DeepFakeDetection/',
-#         }
-#         self.name = config.name
-#         self.root = path.expanduser(config.root_dir)
-#         self.detection_level = config.detection_level
-#         self.types = config.types
-#         self.compressions = config.compressions
-#         self.num_frames = num_frames
-#         self.clip_duration = clip_duration
-#         self.split = split
-#         self.transform = transform
-#         if accelerator:
-#             with accelerator.main_process_first():
-#                 self._build_video_table(accelerator)
-#         else:
-#             self._build_video_table(accelerator)
-#         self._build_data_list(accelerator)
-
-#     def __len__(self):
-#         return len(self.data_list)
-
-#     def __getitem__(self, idx):
-#         if self.split == 'test':
-#             # if at testing, return all 1-sec clips of a video
-#             # TODO: frame-level testing
-#             return self._get_test_item(idx)
-
-#         df_type, comp, idx = self.data_list[idx]
-#         video_clips = self.video_table[df_type][comp][idx]
-#         clip  = random.choice(video_clips)
-#         reader = VideoReader(path.join(self.root, self.TYPE_DIRS[df_type], comp, 'videos', idx, f'{clip}.avi'), "video")
-#         metadata = reader.get_metadata()
-#         if self.detection_level == 'frame':
-#             frame = None
-#             n_tries = 0
-#             while frame is None:
-#                 n_tries += 1
-#                 reader.seek(random.uniform(0, metadata['video']['duration'][0]) if n_tries < 3 else 0)
-#                 try:
-#                     frame = next(reader)['data']
-#                 except StopIteration:
-#                     pass
-
-#             # apply transform
-#             frame = self.transform(frame)
-
-#             return frame, 1 if df_type == 'REAL' else 0
-
-#         elif self.detection_level == 'video':
-#             # TODO: random select start time frame
-#             frames = []get_metadata
-#                 frames.append(frame['data'])
-
-#             frames = self.transform(torch.stack(frames))
-#             mask = torch.tensor([1.] * len(frames) +
-#                                 [0.] * (self.num_frames - len(frames)), dtype=torch.bool)
-
-#             if len(frames) < self.num_frames:
-#                 diff = self.num_frames - len(frames)
-#                 padding = torch.zeros((diff, *frames.shape[1:]))
-#                 frames = torch.concatenate((frames, padding))
-
-#             return frames, 1 if df_type == 'REAL' else 0, mask
-#         else:
-#             raise NotImplementedError
-
-#     def _build_video_table(self, accelerator):
-#         self.video_table = {}
-
-#         progress_bar = tqdm(self.types, disable=not accelerator.is_local_main_process)
-#         for df_type in progress_bar:
-#             self.video_table[df_type] = {}
-#             for comp in self.compressions:
-#                 video_cache = path.expanduser(f'~/.cache/dfd-clip/videos/{df_type}-{comp}.pkl')
-#                 # if path.isfile(video_cache):
-#                 #     with open(video_cache, 'rb') as f:
-#                 #         videos = pickle.load(f)
-#                 #     self.video_table[df_type][comp] = videos
-#                 #     continue
-
-#                 subdir = path.join(self.root, self.TYPE_DIRS[df_type], '')
-#                 # video table
-#                 videos = {f.name: [] for f in scandir(path.join(subdir, f'{comp}/videos')) if f.is_dir()}
-#                 print(subdir,videos)
-#                 for fname in videos:
-#                     # clip list
-#                     videos[fname] = [
-#                         f.name[:-4] for f in scandir(path.join(subdir, f'{comp}/videos', fname)) if '.avi' in f.name]
-#                     progress_bar.set_description(f"{df_type}: {path.join(f'{comp}/videos', fname)}")
-
-#                 if accelerator.is_local_main_process:
-#                     makedirs(path.dirname(video_cache), exist_ok=True)
-#                     with open(video_cache, 'wb') as f:
-#                         pickle.dump(videos, f)
-
-#                 self.video_table[df_type][comp] = videos
-        
-#     def _build_data_list(self, accelerator):
-#         self.data_list = []
-        
-#         with open(path.join(self.root, 'splits', f'{self.split}.json')) as f:
-#             idxs = json.load(f)
-            
-#         for df_type in self.types:
-#             for comp in self.compressions:
-#                 adj_idxs = [i for inner in idxs for i in inner] if df_type == 'REAL' else ['_'.join(idx) for idx in idxs] + ['_'.join(reversed(idx)) for idx in idxs]
-
-#                 for idx in adj_idxs:
-#                     if idx in self.video_table[df_type][comp]:
-#                         self.data_list.append((df_type, comp, idx))
-#                     else:
-#                         accelerator.print(f'Warning: video {path.join(self.root, self.TYPE_DIRS[df_type], comp, "videos", idx)} does not present in the processed dataset.')
-
-#     def _get_test_item(self, idx):
-#         df_type, comp, idx = self.data_list[idx]
-#         video_clips = self.video_table[df_type][comp][idx]
-#         if self.detection_level == 'frame':
-#             raise NotImplementedError
-#         elif self.detection_level == 'video':
-#             clips = []
-#             masks = []
-#             for clip in video_clips:
-#                 reader = VideoReader(path.join(self.root, self.TYPE_DIRS[df_type], comp, 'videos', idx, f'{clip}.avi'), "video")
-#                 frames = []
-#                 count = 0
-#                 for frame in reader:
-#                     count += 1
-#                     if count > self.num_frames:
-#                         break
-#                     frames.append(frame['data'])
-
-#                 frames = self.transform(torch.stack(frames))
-#                 mask = torch.tensor([1.] * len(frames) +
-#                                     [0.] * (self.num_frames - len(frames)), dtype=torch.bool)
-
-#                 if len(frames) < self.num_frames:
-#                     diff = self.num_frames - len(frames)
-#                     padding = torch.zeros((diff, *frames.shape[1:]))
-#                     frames = torch.concatenate((frames, padding))
-
-#                 clips.append(frames)
-#                 masks.append(mask)
-
-#             return clips, 1 if df_type == 'REAL' else 0, masks
-#         else:
-#             raise NotImplementedError
-
-
 class SessionMeta:
-    def __init__(self,session_dir,save_gae=False,save_xml=False,cropped_dir=None):
+    def __init__(self,session_dir,save_gae=False,save_xml=False):
         self.session_dir = session_dir
         self.session_path = path.join(session_dir,"session.xml")
-        self.cropped_dir = cropped_dir
         self.xml = None
         self.session_video_beg_sample=None
         self.session_video_end_sample=None
@@ -230,7 +58,7 @@ class SessionMeta:
         for l1_tag in self.xml:
             if "color" in l1_tag.attrib and  l1_tag.attrib["color"] == "1":
                 self.video_path = path.join(
-                    self.session_dir if not cropped_dir else self.cropped_dir, l1_tag.attrib["filename"]
+                    self.session_dir, l1_tag.attrib["filename"]
                 )
                 for l2_tag in l1_tag: 
                     if("type" in l2_tag.attrib and l2_tag.attrib["type"] == "Gaze"):
@@ -319,9 +147,13 @@ class SessionMeta:
     def video_length(self):
         return self.session_video_end_sample - self.session_video_beg_sample + 1
 
-    def require_check(self,video=False, gae=False, bdf=False,time=False):
-        if(video and not (self.video_path and path.exists(self.video_path))):
-            return False
+    def require_check(self,video=False, gae=False, bdf=False,time=False,video_folders=["cropped_faces"]):
+        if(video):
+            if(not self.video_path or not path.exists(self.video_path)):
+                return False
+            for folder in video_folders:
+                if(not path.exists(self.video_path.replace("Sessions",folder))):
+                    return False
         if(gae and type(self.gae_data)==type(None)):
             return False
         if(bdf and not (self.bdf_path and path.exists(self.bdf_path))):
@@ -330,7 +162,6 @@ class SessionMeta:
             return False
 
         return True
-
 
 class FFPP(Dataset):
     @staticmethod
@@ -460,7 +291,8 @@ class FFPP(Dataset):
                 df_type, comp, video_name, clips = self.video_list[video_idx]
                 video_meta = self.video_table[df_type][comp][video_name]
                 video_offset_duration =  (idx - (0 if video_idx == 0 else self.stack_video_clips[video_idx-1]))*self.clip_duration
-
+                logging.debug(f"idx:{idx}, video_idx:{video_idx},df_type:{df_type},comp:{comp},video_name:{video_name}")
+                logging.debug(f"video_meta:{video_meta}")
                 # video frame processing
                 frames = []
                 cap = cv2.VideoCapture(video_meta["path"])
@@ -508,7 +340,6 @@ class FFPP(Dataset):
                 logging.error(f"Error occur: {e}")
                 idx = random.randrange(0,len(self))
 
-
 class RPPG(Dataset):
     @staticmethod
     def get_default_config():
@@ -521,11 +352,13 @@ class RPPG(Dataset):
         C.cropped_folder="cropped_faces"
         C.meta_folder="Metas"
         C.dataset="RPPG"
+        C.compressions = ["raw"]
         return C
 
     def __init__(self, config,num_frames,clip_duration, transform=None, accelerator=None, split='train',index=0):
         assert 0 <= config.scale <= 1
         assert 0 <= config.train_ratio <= 1
+        assert split in ["train","val"]
 
         # TODO: accelerator not implemented
         self.name = config.name
@@ -535,6 +368,8 @@ class RPPG(Dataset):
         self.clip_duration = clip_duration
         self.index = index
         self.scale = config.scale
+        self.compressions = config.compressions
+        self.cropped_folder =  config.cropped_folder
         
         # dataset consistency
         rng = random.Random()
@@ -550,24 +385,37 @@ class RPPG(Dataset):
         
         # speed up dataset initialization
         if (not config.meta_folder):
+            logging.info("Meta folder unspecified, building session meta infos....")
             self.session_metas = [
                 SessionMeta(
-                    session_dir,
-                    cropped_dir=session_dir.replace("Sessions",config.cropped_folder) if config.cropped_folder else None 
+                    session_dir
                 ) 
                 for session_dir in target_sessions
             ]
         else:
-            self.session_metas = [None for _ in target_sessions]
-            for i,session_dir in enumerate(target_sessions):
+            logging.info("Meta folder specified, loading meta infos....")
+            self.session_metas = [None for _ in range(len(target_sessions))]
+            for i,session_dir in tqdm(enumerate(target_sessions)):
                 try:
                     with open(path.join(session_dir.replace("Sessions",config.meta_folder),"meta.pickle"),"rb") as file:
                         self.session_metas[i] = pickle.load(file)
                 except Exception as e:
                     logging.debug(f"Error while loading meta pickle: '{e}'")
 
-        # remove erroneous datas.
-        self.session_metas = [meta for meta in self.session_metas if meta and meta.require_check(video=True,bdf=True,time=True)]
+        logging.info("Session meta created.")
+        logging.debug(f"Number of session metas before checking: {len(self.session_metas)}")
+        # remove erroneous/missing datas.
+        for comp in self.compressions:
+            self.session_metas = [
+                meta for meta in self.session_metas 
+                if meta and meta.require_check(
+                    video=True,
+                    bdf=True,
+                    time=True,
+                    video_folders=[path.join(config.cropped_folder,comp)]
+                )
+            ]
+        logging.debug(f"Number of session metas after checking: {len(self.session_metas)}")
 
         # calculate available clips per session
         self.session_clips = [int(meta.duration // self.clip_duration) for meta in self.session_metas]
@@ -579,7 +427,7 @@ class RPPG(Dataset):
         self.stack_session_clips.pop(0)
 
     def __len__(self):
-        return self.stack_session_clips[-1]
+        return self.stack_session_clips[-1]*len(self.compressions)
     
     def __getitem__(self,idx):
         result = self.get_dict(idx)
@@ -588,6 +436,8 @@ class RPPG(Dataset):
     def get_dict(self,idx):
         while(True):
             try:
+                comp = self.compressions[int(idx //self.stack_session_clips[-1])]
+                idx = idx % self.stack_session_clips[-1]
                 session_idx =  next(i for i,x in enumerate(self.stack_session_clips) if  idx < x)
                 session_meta = self.session_metas[session_idx]
                 session_offset_duration =  (idx - (0 if session_idx == 0 else self.stack_session_clips[session_idx-1]))*self.clip_duration
@@ -635,12 +485,15 @@ class RPPG(Dataset):
                 best_pair = sorted(_hr_datas,key=lambda x : x[1]["sdnn"])[0]
                 hr_data,measures,wd = best_pair[0], best_pair[1], best_pair[2]
                 bpm = measures["bpm"]
-                1/(pow(2*math.pi,0.5))*pow(math.e,(-pow((31-(72-41)),2)/2))
                 label = torch.tensor([1/(pow(2*math.pi,0.5))*pow(math.e,(-pow((k-(bpm-41)),2)/2)) for k in range(180)])
 
                 # video frame processing
                 frames = []
-                cap = cv2.VideoCapture(session_meta.video_path)
+                comp_video_path = session_meta.video_path.replace(
+                    "Sessions",
+                    path.join("Sessions" if not self.cropped_folder else self.cropped_folder,comp)
+                )
+                cap = cv2.VideoCapture(comp_video_path)
                 assert int(session_meta.session_video_sample_freq) == int(cap.get(cv2.CAP_PROP_FPS)), f"{int(session_meta.session_video_sample_freq)},{int(cap.get(cv2.CAP_PROP_FPS))}"
                 video_sample_freq = session_meta.session_video_sample_freq
                 # - the amount of frames to skip
@@ -648,10 +501,11 @@ class RPPG(Dataset):
                 # - the amount of frames for the duration of a clip
                 video_clip_samples = int(session_meta.session_video_sample_freq * self.clip_duration)
                 # - the amount of frames to skip in order to meet the num_frames per clip.(excluding the head & tail frames )
-                video_sample_stride = video_clip_samples / (self.num_frames - 1)
+                video_sample_stride = (video_clip_samples-1) / (self.num_frames - 1)
                 # - fast forward to the the sampling start.
                 cap.set(cv2.CAP_PROP_POS_FRAMES,video_sample_offset)
                 # - fetch frames of clip duration
+                logging.debug(f"Loading video: {comp_video_path}, sample offset:{video_sample_offset}")
                 next_sample_idx = 0
                 for sample_idx in range(video_clip_samples):
                     ret, frame = cap.read()
