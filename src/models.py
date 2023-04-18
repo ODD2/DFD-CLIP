update configurations for correct number of rPPG dimensions 2. fix incorrect lmse, missing softmax operation

src/models.py
@@ -8,7 +8,7 @@ import torchvision.transforms as T
 
 def lmse(logits,y):
     b,w = logits.shape
-    return  torch.log(torch.pow(logits @ torch.tensor([ i for i in range(w)]).float().to(logits.device) - y,2))
+    return  torch.log(torch.pow(logits.softmax(dim=-1) @ torch.tensor([ i for i in range(w)]).float().to(logits.device) - y,2))
 
 def kl_div(logits,y):
     return torch.nn.functional.kl_div(torch.nn.functional.log_softmax(logits,dim=1), y, reduction='none')