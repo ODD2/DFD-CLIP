8aac2d9242adfd068efaf6cd7f51883d0e810f5f
OD <vhanxtreme@gmail.com>
2023-08-09 22:20:18 +0800

    create clip_length ablation configurations & script

---
+++ scripts/ablate-clip-length.sh
@@ -0,0 +1,4 @@
+python -m main --cfg configs/train/clip_length/1s.yaml --notes "baseline(1s)";\
+python -m main --cfg configs/train/clip_length/2s.yaml --notes "baseline(2s)";\
+python -m main --cfg configs/train/clip_length/3s.yaml --notes "baseline(3s)";\
+python -m main --cfg configs/train/clip_length/5s.yaml --notes "baseline(5s)";
