11e1ba01f511ea8fe1ff6468841c0bb455c3bdf1
OD <vhanxtreme@gmail.com>
2023-08-10 10:03:57 +0800

    fix decoder layer script

--- scripts/ablate-decode-layer.sh
+++ scripts/ablate-decode-layer.sh
@@ -1,5 +1,5 @@
-python -m main --cfg configs/train/decoder_layer/1dl.yaml --notes "baseline(1dl)";\
-python -m main --cfg configs/train/decoder_layer/2dl.yaml --notes "baseline(2dl)";\
-python -m main --cfg configs/train/decoder_layer/3dl.yaml --notes "baseline(3dl)";\
-python -m main --cfg configs/train/decoder_layer/4dl.yaml --notes "baseline(4dl)";\
-python -m main --cfg configs/train/decoder_layer/5dl.yaml --notes "baseline(5dl)";
+python -m main --cfg configs/train/decode_layer/1dl.yaml --notes "baseline(1dl)";\
+python -m main --cfg configs/train/decode_layer/2dl.yaml --notes "baseline(2dl)";\
+python -m main --cfg configs/train/decode_layer/3dl.yaml --notes "baseline(3dl)";\
+python -m main --cfg configs/train/decode_layer/4dl.yaml --notes "baseline(4dl)";\
+python -m main --cfg configs/train/decode_layer/5dl.yaml --notes "baseline(5dl)";
