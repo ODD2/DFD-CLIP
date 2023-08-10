432b7988273270381578266eacccbed0a6840a0c
OD <vhanxtreme@gmail.com>
2023-08-10 14:22:59 +0800

    adjust ablatios for prompt_layer due to insufficient memory.

--- scripts/ablate-prompt-layer.sh
+++ scripts/ablate-prompt-layer.sh
@@ -1,4 +1,5 @@
 python -m main --cfg configs/train/prompt_layer/1pl.yaml --notes "baseline(1pl)";\
+python -m main --cfg configs/train/prompt_layer/2pl.yaml --notes "baseline(2pl)";\
 python -m main --cfg configs/train/prompt_layer/3pl.yaml --notes "baseline(3pl)";\
-python -m main --cfg configs/train/prompt_layer/9pl.yaml --notes "baseline(9pl)";\
-python -m main --cfg configs/train/prompt_layer/12pl.yaml --notes "baseline(12pl)";
+python -m main --cfg configs/train/prompt_layer/4pl.yaml --notes "baseline(4pl)";\
+python -m main --cfg configs/train/prompt_layer/5pl.yaml --notes "baseline(5pl)";
