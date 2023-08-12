15e4bc57af3792c8f3c2d65595d0d16d395fcbfc
OD <vhanxtreme@gmail.com>
2023-08-13 02:02:52 +0800

    ablation for components

---
+++ scripts/ablate-components.sh
@@ -0,0 +1,5 @@
+python -m main --cfg configs/train/components/frozen+patch.yaml --notes "patch_attn";\
+python -m main --cfg configs/train/components/frozen+patch_frame.yaml --notes "(patch+frame)_attn";\
+python -m main --cfg configs/train/components/frozen+patch_frame+guide.yaml --notes "(patch+frame)_attn+guide";\
+python -m main --cfg configs/train/components/frozen+patch_frame+guide+prompt.yaml --notes "(patch+frame)_attn+guide+prompt";\
+python -m main --cfg configs/train/components/frozen+patch_frame+guide+prompt+frame_task.yaml --notes "(patch+frame)_attn+guide+prompt+frame_task";
